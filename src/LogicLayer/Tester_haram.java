5af6df0d0bff525019a1321859b9beff65be50ce
Tanzyl <f200425@cfd.nu.edu.pk>
2023-01-04 11:22:33 +0500

    query moved from update word to database
    commit

---
+++ src/LogicLayer/Tester_haram.java
@@ -0,0 +1,44 @@
+package LogicLayer;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+import DataAccessLayer.Database_haram;
+
+class Tester_haram {
+	@Test
+	public void TestingTextGetter()
+	{
+		UrduString_haram obj = new UrduString_haram();
+		String word = obj.getTxt();
+		Assert.assertTrue(word == "");
+	}
+	
+	@Test
+	public void TestingObject()
+	{
+		Inputtext_haram obj = new Inputtext_haram();
+	Assert.assertNotNull(obj);
+    }
+	
+	@Test
+	public void TestingConnection() throws SQLException
+	{
+		Database_haram db = new Database_haram();
+		Connection conn=DriverManager.getConnection("jdbc:mysql://localhost:3306/spellchecker?useSSL=false","root","");
+		Connection testerConnection = db.dbconnect();
+		Assert.assertEquals(conn, testerConnection);
+	}
+	
+	@Test
+	public void TestingFirstWord()
+	{
+		UrduString_haram obj = new UrduString_haram();
+		String word = obj.getFirstWord();
+		Assert.assertEquals(word, "Haram");
+	}
+}
