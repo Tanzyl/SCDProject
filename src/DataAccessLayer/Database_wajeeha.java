0c7ac4dc24eaf5d391b16cc36af03e7fa19a9725
WajeehaKhalid1 <f201096@cfd.nu.edu.pk>
2023-01-04 05:45:28 +0500

    fixed database_wajeeha

--- src/DataAccessLayer/Database_wajeeha.java
+++ src/DataAccessLayer/Database_wajeeha.java
@@ -1,21 +1,36 @@
 package DataAccessLayer;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import java.sql.*;
-import javax.swing.JOptionPane;
-//original name was Database
-public class Database_wajeeha {
-	public static Connection dbconnect() {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			String username = "root";
-			String password = "";
-			String url = "jdbc:mysql://localhost:3306/spellchecker?useSSL=false";
-		Connection 	conn = DriverManager.getConnection(url, username, password);
-			return conn;
-		}catch(Exception e) {
-			JOptionPane.showMessageDialog(null, e);
-			return null;
-		}
-	}
+public class Database {
+
+    private static Database instance;
+    private Connection connection;
+    private String url = "jdbc:mysql://localhost:3306/spellchecker?useSSL=false";
+    private String username = "root";
+    private String password = "";
+
+    private Database() throws SQLException {
+        try {
+            
+            this.connection = DriverManager.getConnection(url, username, password);
+        } catch (Exception ex) {
+            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static Database getInstance() throws SQLException {
+        if (instance == null) {
+            instance = new Database();
+        } else if (instance.getConnection().isClosed()) {
+            instance = new Database();
+        }
+
+        return instance;
+    }
 }
-
