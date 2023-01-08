64b55dbf63ad9eada86be856ce1a8712d65c0c95
Tanzyl <f200425@cfd.nu.edu.pk>
2023-01-08 17:32:23 +0500

    Updated project

--- UseCase1Implementation/src/LogicLayer/Fascade.java
+++ UseCase1Implementation/src/LogicLayer/Fascade.java
@@ -9,14 +9,14 @@ public class Fascade {
 	Mutants mutants = new Mutants();
 	
 	public boolean replaceAndInsert(String word,int idx,String[]checkArray,int id) {
-		return mutants.replaceAndInsert(word, idx, checkArray, id);
+		return Mutants.replaceAndInsert(word, idx, checkArray, id);
 	}
 	public void checkMutant(String word,int id,int index) {
-		mutants.checkMutant(word, id, index);
+		Mutants.checkMutant(word, id, index);
 	}
 	public void insertion (int idx, String[] checkArray, int id, int i, String new_word) {
 		try {
-			mutants.insertion(idx, checkArray, id, i, new_word);
+			Mutants.insertion(idx, checkArray, id, i, new_word);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -25,15 +25,15 @@ public class Fascade {
 	
 	public void getMutants() {
 		MutantDataAccess mda = new MutantDataAccess();
-	mda.getMutants();
+	MutantDataAccess.getMutants();
 	}
 	
 	public void insertMutant(int id, String n) throws SQLException {
 		MutantDataAccess mda = new MutantDataAccess();
-		mda.insertMutant(id, n);
+		MutantDataAccess.insertMutant(id, n);
 	}
 	public void insertCreate() throws SQLException {
 		MutantDataAccess mda = new MutantDataAccess();
-		mda.insertCreate();
+		MutantDataAccess.insertCreate();
 	}
 }
